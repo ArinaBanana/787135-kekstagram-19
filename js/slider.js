ab7a0f58119b434b1193068554c657aaeb99f5e4
Arina <hadson.rey@mail.ru>
2020-03-17 21:48:07 +0300

    реализовывает перетаскивание, взаимодействие с фильтрами

--- js/slider.js
+++ js/slider.js
@@ -1,45 +1,78 @@
 'use strict';
 
 window.slider = (function () {
-  var sliderRange = document.querySelector('.effect-level__line');
-  var sliderToggle = sliderRange.querySelector('.effect-level__pin');
-  var sliderControlDepth = sliderRange.querySelector('.effect-level__depth');
+  var getSlider = function (effectLevel, changeHandler) {
+    var sliderRange = effectLevel.querySelector('.effect-level__line');
+    var sliderToggle = effectLevel.querySelector('.effect-level__pin');
+    var sliderControlDepth = effectLevel.querySelector('.effect-level__depth');
 
-  sliderToggle.addEventListener('mousedown', function (evt) {
-    var startCoords = {
-      x: evt.clientX
+    var sliderInputValue = effectLevel.querySelector('.effect-level__value');
+
+    var getValue = function () {
+      return sliderInputValue.value;
     };
 
-    var moveHandler = function (moveEvt) {
-      var coordsContainer = {
-        left: 0,
-        right: sliderToggle.offsetParent.offsetWidth // 453
-      };
+    var getDefaultPositionToggle = function () {
+      var positionDefault = '453px';
 
-      var shift = {
-        x: startCoords.x - moveEvt.clientX
-      };
-
-      startCoords = {
-        x: moveEvt.clientX
-      };
-
-      if (shift.x === coordsContainer.left && shift.x === coordsContainer.right) {
-        sliderToggle.style.left = (sliderToggle.offsetLeft - shift.x) + 'px';
-        sliderControlDepth.style.width = (sliderToggle.offsetLeft - shift.x) + 'px';
-      }
-
-      console.log(sliderToggle.offsetParent.offsetWidth);
+      sliderToggle.style.left = positionDefault;
+      sliderControlDepth.style.width = positionDefault;
     };
 
-    var upHandler = function () {
-      document.removeEventListener('mousemove', moveHandler);
-      document.removeEventListener('mouseup', upHandler);
+    var showSlider = function () {
+      effectLevel.classList.remove('hidden');
     };
 
-    document.addEventListener('mousemove', moveHandler);
-    document.addEventListener('mouseup', upHandler);
+    var hideSlider = function () {
+      effectLevel.classList.add('hidden');
+    };
 
-  });
+    sliderToggle.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+
+      var sliderClientRect = sliderRange.getBoundingClientRect();
+
+      var moveHandler = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        var togglePosition = moveEvt.clientX - sliderClientRect.left;
+
+        if (togglePosition < 0) {
+          togglePosition = 0;
+        } else if (togglePosition > sliderRange.offsetWidth) {
+          togglePosition = sliderRange.offsetWidth;
+        }
+
+        sliderToggle.style.left = togglePosition + 'px';
+        sliderControlDepth.style.width = togglePosition + 'px';
+
+        var togglePositionPercent = togglePosition / sliderRange.offsetWidth;
+
+        changeHandler(togglePositionPercent);
+        sliderInputValue.setAttribute('value', togglePositionPercent);
+      };
+
+      var upHandler = function () {
+        document.removeEventListener('mousemove', moveHandler);
+        document.removeEventListener('mouseup', upHandler);
+      };
+
+      document.addEventListener('mousemove', moveHandler);
+      document.addEventListener('mouseup', upHandler);
+
+    });
+
+    return {
+      getValue: getValue,
+      getDefaultPositionToggle: getDefaultPositionToggle,
+      showSlider: showSlider,
+      hideSlider: hideSlider
+    };
+  };
+
+  return {
+    getSlider: getSlider
+  };
 
 })();
+
