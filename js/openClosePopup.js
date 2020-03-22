0e051652f9e4f258d5259cbe0b6b7783d6c4d0cc
Arina <hadson.rey@mail.ru>
2020-03-22 14:31:50 +0300

    убирает лишнее из названий (модуль popup)

--- js/openClosePopup.js
+++ js/openClosePopup.js
@@ -3,20 +3,20 @@
 window.openClosePopup = (function () {
   var body = document.querySelector('body');
 
-  var openPopup = function (domElement, pressEscapeHandler) {
+  var open = function (domElement, pressEscapeHandler) {
     domElement.classList.remove('hidden');
     document.addEventListener('keydown', pressEscapeHandler);
     body.classList.add('modal-open');
   };
-  var closePopup = function (domElement, pressEscapeHandler) {
+  var close = function (domElement, pressEscapeHandler) {
     domElement.classList.add('hidden');
     document.removeEventListener('keydown', pressEscapeHandler);
     body.classList.remove('modal-open');
   };
 
   return {
-    openPopup: openPopup,
-    closePopup: closePopup
+    open: open,
+    close: close
   };
 
 })();
